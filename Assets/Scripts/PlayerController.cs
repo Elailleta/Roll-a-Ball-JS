0c435d24408356ec2832f685a6ab5e513a88a5d2
Elailleta <jessica.stride@ocsbstudent.ca>
2017-09-19 09:48:52 -0400

    added rotate and pick up function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,14 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
 
+    private int count;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
     }
 
     void FixedUpdate()
@@ -21,5 +26,13 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
